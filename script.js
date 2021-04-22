91e2cde8bce7d2308380a14d5a1acd991071b13c
Iruiasaro <brunettirosario@gmail.com>
2021-04-22 16:10:56 +0200

    creazione array studenti e stampa

--- script.js
+++ script.js
@@ -2,11 +2,39 @@
 var studentIdentity = {
     nome: "James",
     cognome: "LeBron",
-    età: "36",
+    age: 36,
 }
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for (var key in studentIdentity) {
     console.log(studentIdentity[key]);
   }
 
+ //Creare un array di oggetti di studenti.
+var studenti = [
+    {
+        nome: "Micheal",
+        cognome: "Jordan",
+        age: 58,    
+    },
+    
+    {
+        nome: "Karl",
+        cognome: "Malone",
+        age: 57,    
+    },
+
+    {
+        nome: "Shaquille",
+        cognome: "O'Neal",
+        age: 49,    
+    },
+];
+
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (var i = 0; i < studenti.length; i++) {
+    var studentIdentity = studenti[i];
+    console.log(studentIdentity.nome, studentIdentity.cognome);
+  };
+
   
+
